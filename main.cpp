af8ec3909ce8aa2013d047148eab781e7e8999fb
Reets123 <ibrahimalatig@gmail.com>
2024-12-10 02:41:55 +0000

    commit 1

--- main.cpp
+++ main.cpp
@@ -10,9 +10,27 @@
 
 using namespace std;
 
-// Struct for Coffee Booth Node
+// Struct for Coffee Booth 
 struct CoffeeNode {
     string customerName;
     string drinkOrder;
     CoffeeNode* next;
+};
+
+// Struct for Muffin Booth
+struct MuffinCustomer {
+    string customerName;
+    string muffinOrder;
+};
+
+// Struct for Friendship Bracelet 
+struct BraceletCustomer {
+    string customerName;
+    string braceletOrder;
+};
+
+// Struct for Candies 
+struct CandyCustomer {
+    string customerName;
+    strin candyOrder;
 };
